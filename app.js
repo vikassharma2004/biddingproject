e35b0c7bea7482c410f75db3b23bd4d2849cca91
Vikas Sharma <vikassharma2004@example.com>
2025-02-09 20:29:23 +0530

    fixed an minor bug

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 import express from "express";
-import cluster from "node:cluster";
-import { availableParallelism } from "node:os";
-import process from "node:process";
 import { config } from "dotenv";
 import fileupload from "express-fileupload";
 import { connection } from "./db/connection.js";
@@ -12,34 +9,13 @@ import UserRouter from "./routes/User.Router.js";
 import AuctionRouter from "./routes/Auction.route.js";
 import BidsRouter from "./routes/Bid.route.js";
 import CommisonRoute from "./routes/Commision.route.js";
-//  fork process
+
+// Load environment variables
+config({ path: "./config/config.env" });
+
 const app = express();
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
 
-  // Fork workers based on the number of CPUs
-  const numCPUs = availableParallelism();
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  // Handle worker exit events
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(
-      `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
-    );
-    console.log("Forking a new worker...");
-    cluster.fork(); // Fork a new worker to replace the dead one
-  });
-} else {
-  // Worker process
-  console.log(`Worker ${process.pid} started`);
-}
-
-config({
-  path: "./config/config.env",
-});
-
+// Middleware setup
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
@@ -56,26 +32,22 @@ app.use(
     tempFileDir: "/tmp",
   })
 );
+
+// Routes
 app.use("/api/v1/user", UserRouter);
 app.use("/api/v1/auctions", AuctionRouter);
 app.use("/api/v1/bids", BidsRouter);
 app.use("/api/v1/commison", CommisonRoute);
+
+// Database connection
 connection();
+
+// Global error handling middleware
 app.use(errorMiddleware);
+
+// Health check endpoint
 app.get("/", (req, res) => {
-  try {
-    res.status(200).json({ message: `server running success full${process.pid} `});
-  } catch (error) {
-    console.error(`Error in worker ${process.pid}:`, error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  res.status(200).json({ message: `Server is running successfully` });
 });
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at", promise, "reason:", reason);
-  process.exit(1);
-});
-
-
 
 export default app;
