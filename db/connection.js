d1e506b2acbc87c9ff5545b7fff188715ddff45c
Vikas Sharma <vikassharma2004@example.com>
2025-02-09 18:14:15 +0530

    optimised the db

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ export const connection = () => {
   try {
     mongoose.connect(process.env.MONGO_URL,{ 
    
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 10000,
       socketTimeoutMS: 45000,});
     console.log("Database connected");
     
